019477f7f11ee54ca86692913489585a7a9d9b41
A.G.Sandun Sankalpa <sandunsankalpa10@gmail.com>
2024-05-23 01:08:31 +0530

    created components

---
+++ src/assets/Components/SectionWrapper.jsx
@@ -0,0 +1,10 @@
+import React, { Children } from 'react'
+
+export default function SectionWrapper(props) {
+    const {children} = props
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
