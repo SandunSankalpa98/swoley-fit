39690e5106fde86ea3ce20d46a68951a436ae99a
A.G.Sandun Sankalpa <sandunsankalpa10@gmail.com>
2024-05-29 00:10:44 +0530

    Finished

--- src/Components/ExcerciseCard.jsx
+++ src/Components/ExcerciseCard.jsx
@@ -4,21 +4,22 @@ import PropTypes from 'prop-types';
 export default function ExerciseCard(props) {
     const { exercise, i } = props;
 
-    const [setsCompleted, setSetsCompleted] = useState(0)
-    function handleSetIncreament() {
-
-        setSetsCompleted((setsCompleted + 1) % 6)
+    const [setsCompleted, setSetsCompleted] = useState(0);
+    
+    function handleSetIncrement() {
+        setSetsCompleted((setsCompleted + 1) % 6);
     }
+
     return (
         <div className='p-4 rounded-md flex flex-col gap-4 bg-slate-950 sm:flex-wrap'>
             <div className="flex flex-col sm:flex-row sm:items-center sm:flex-wrap gap-x-4">
-                <h4 className='text-3xl hidden sm:inline sm:text-4xl md:text-5xl font-semibold to-slate-400'>
+                <h4 className='text-3xl hidden sm:inline sm:text-4xl md:text-5xl font-semibold text-slate-400'>
                     0{i+1}
                 </h4>
                 <h2 className="capitalize whitespace-nowrap truncate max-w-full text-lg sm:text-xl md:text-2xl flex-1 sm:text-center">
                     {exercise.name.replaceAll("_", " ")}
                 </h2>
-                <p className="capitalize text-sm to-slate-400">{exercise.type}</p>
+                <p className="capitalize text-sm text-slate-400">{exercise.type}</p>
             </div>
 
             <div className='flex flex-col'>
@@ -26,29 +27,24 @@ export default function ExerciseCard(props) {
                 <p className="capitalize">{exercise.muscles.join(' & ')}</p>
             </div>
 
-            <div className='flex flex-col bg-slate-950 rounded gap-2 '>
-                {exercise.description.split('___').map((val) => {
-                    return (
-                        <div className='text-sm'>
-                            {val}
-                        </div>
-                    )
-                })}
+            <div className='flex flex-col bg-slate-950 rounded gap-2'>
+                {exercise.description.split('___').map((val, index) => (
+                    <div key={index} className='text-sm'>
+                        {val}
+                    </div>
+                ))}
             </div>
 
             <div className="grid grid-cols-2 sm:grid-cols-4 sm:place-items-center gap-2">
-                {['reps','rest' ,'tempo'].map(info => {
-                    return(
-                        <div key={info} className='flex flex-col p-2 rounded border-[1.5px] border-solid border-slate-900 w-full'>
-                            <h3 className="capitalize text-slate-400 text-sm "> {info === 'reps' ? `${exercise.unit}` : info} </h3>
-                            <p className="font-medium">{exercise[info]}</p>
-                        </div>
-                    )
-                })}
-                <button  onClick={handleSetIncreament} className='flex flex-col p-2 rounded border-[1.5px]  border-solid border-blue-900
-                 hover:border-blue-600 w-full duration-200'>
-                        <h3 className='text-slate-400 text-sm capitalize'>Sets Completed</h3>
-                        <p className='font-medium'>{setsCompleted}/ 5</p>
+                {['reps', 'rest', 'tempo'].map(info => (
+                    <div key={info} className='flex flex-col p-2 rounded border-[1.5px] border-solid border-slate-900 w-full'>
+                        <h3 className="capitalize text-slate-400 text-sm">{info === 'reps' ? `${exercise.unit}` : info}</h3>
+                        <p className="font-medium">{exercise[info]}</p>
+                    </div>
+                ))}
+                <button onClick={handleSetIncrement} className='flex flex-col p-2 rounded border-[1.5px] border-solid border-blue-900 hover:border-blue-600 w-full duration-200'>
+                    <h3 className='text-slate-400 text-sm capitalize'>Sets Completed</h3>
+                    <p className='font-medium'>{setsCompleted}/ 5</p>
                 </button>
             </div>
         </div>
@@ -56,5 +52,15 @@ export default function ExerciseCard(props) {
 }
 
 ExerciseCard.propTypes = {
-    exercise: PropTypes.object.isRequired // Adjust the type based on the expected shape of the exercise prop
+    exercise: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        muscles: PropTypes.arrayOf(PropTypes.string).isRequired,
+        description: PropTypes.string.isRequired,
+        reps: PropTypes.number,
+        rest: PropTypes.string,
+        tempo: PropTypes.string,
+        unit: PropTypes.string
+    }).isRequired,
+    i: PropTypes.number.isRequired
 };
